ebadae7095d774dc97136a3600555468d1c9d480
martino <martino.trevisan@studenti.polito.it>
2018-10-29 15:02:07 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ except ImportError:
 
 setup(
     name="fastplot",
-    description="Create publication-quality plots with a simple interface over matplotlib.",
+    description="Are you bored of re-writing code for each plot? Create publication-quality plots with a simple interface over matplotlib.",
     license="GNU GENERAL PUBLIC LICENSE v3",
-    version="0.1",
+    version="1.0.2",
     author="Martino Trevisan",
     author_email="martino.trevisan@polito.it",
     maintainer="Martino Trevisan",
     maintainer_email="martino.trevisan@polito.it",
     url="https://github.com/marty90/fastplot",
-    download_url = 'https://github.com/marty90/fastplot/tarball/1.0.1',
+    download_url = 'https://github.com/marty90/fastplot/tarball/1.0.2',
     packages=['fastplot'],
     install_requires=['matplotlib', 'numpy', 'pandas', 'statsmodels']
 )
