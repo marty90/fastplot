7fb417101495feb4a60d14e6198c88392dd164cb
Martino Trevisan <martino.trevisan@studenti.polito.it>
2019-04-09 11:54:21 -0300

    added utility functions for gini/lorenz

--- fastplot/__init__.py
+++ fastplot/__init__.py
@@ -280,3 +280,29 @@ def tex_escape(text):
     regex = re.compile('|'.join(re.escape(str(key)) for key in sorted(conv.keys(), key = lambda item: - len(item))))
     return regex.sub(lambda match: conv[match.group()], text)
 
+def gini(arr):
+    count = arr.size
+    coefficient = 2 / count
+    indexes = np.arange(1, count + 1)
+    weighted_sum = (indexes * arr).sum()
+    total = arr.sum()
+    constant = (count + 1) / count
+    return coefficient * weighted_sum / total - constant
+
+def lorenz_gini(arr):
+    arr = np.sort(arr)
+    scaled_prefix_sum = arr.cumsum() / arr.sum()
+    np.insert(scaled_prefix_sum, 0, 0)
+    lorenz_y = scaled_prefix_sum 
+    lorenz_x = np.linspace(0.0, 1.0, lorenz_y.size) 
+    gini_index = gini(arr)
+    return ((lorenz_x, lorenz_y), gini_index)
+
+def lorenz_gini_multi(data, name_format="{} (GI={:0.2f})"):
+    data_new = []
+    for name, samples in data:
+        (lorenz_x, lorenz_y), gini_index = lorenz_gini(samples)
+        name_new = name_format.format(name, gini_index)
+        data_new.append( (name_new, (lorenz_x,lorenz_y) )   )
+    return data_new
+  
