dde4edea9f086251135dd8091ed265af9de6ae8c
martino <martino.trevisan@studenti.polito.it>
2019-10-24 11:53:03 +0200

    published 1.0.6

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setup(
     name="fastplot",
     description="Are you bored of re-writing code for each plot? Create publication-quality plots with a simple interface over matplotlib.",
     license="GNU GENERAL PUBLIC LICENSE v3",
-    version="1.0.5",
+    version="1.0.6",
     author="Martino Trevisan",
     author_email="martino.trevisan@polito.it",
     maintainer="Martino Trevisan",
     maintainer_email="martino.trevisan@polito.it",
     url="https://github.com/marty90/fastplot",
-    download_url = 'https://github.com/marty90/fastplot/tarball/1.0.5',
+    download_url = 'https://github.com/marty90/fastplot/tarball/1.0.6',
     packages=['fastplot'],
     install_requires=['matplotlib', 'numpy', 'pandas', 'statsmodels', 'scipy']
 )
@@ -23,5 +23,4 @@ setup(
 # python setup.py sdist
 # twine upload dist/*
 
-# Install locally with:
-# sudo python3 setup.py install
+# Test it locally removing the pip installed one
